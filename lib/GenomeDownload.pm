66a693e691ecc0e7cde4d4786fba5579c411fbe7
Robert Olson <bob@rdolson.org>
2025-03-08 17:29:03 +0000

    Finish up export by adding suffix map and complete list of types available in seed_rast.

--- lib/GenomeDownload.pm
+++ lib/GenomeDownload.pm
@@ -12,17 +12,45 @@ use FIG;
 
 my $json = JSON::XS->new;
 
-my %allowed = map { $_ => 1 } qw(protein_fasta genbank  patric_features);
+
+my @allowed = qw(
+	genbank gbk
+	genbank_merged gbk
+	spreadsheet_txt txt
+	spreadsheet_xls xls
+	feature_data txt
+	protein_fasta faa
+	contig_fasta fna
+	feature_dna ffn
+	seed_dir seed
+	patric_features txt
+	patric_specialty_genes txt
+	patric_genome_metadata txt
+	gff gff
+	embl embl
+	);
+my @need_contig = qw(
+	genbank
+	genbank_merged
+	contig_fasta
+	seed_dir
+	gff
+	embl
+	);
+my %allowed;
+my %need_contig = map { $_ => 1 } @need_contig;
+my %suffix;
+for (my $i = 0; $i < @allowed; $i += 2)
+{
+    $allowed{$allowed[$i]} = 1;
+    $suffix{$allowed[$i]} = $allowed[$i + 1];
+}
 
 sub get_export
 {
     my($gid, $type) = @_;
 
-    my $params = { };
-    if ($type  ne "genbank")
-    {
-	$params->{skip_contigs} = 1;
-    }
+    my $params = { skip_contigs => !$need_contig{$type} };
 
     my $fig = FIG->new;
     my $gobj = $fig->genome_id_to_genome_object($gid, $params);
@@ -36,6 +64,11 @@ sub get_export
     return $export;
 }
 
+get '/doc' => sub {
+    content_type 'text/plain';
+    return "Allowed types:\n" . join("\n", @allowed) . "\n";
+};
+
 get '/:gid/:type' => sub {
     my $gid = params->{gid};
     my $type = params->{type};
@@ -47,7 +80,7 @@ get '/:gid/:type' => sub {
     my $export = get_export($gid, $type);
 
     send_file( \$export, content_type => 'application/x-fasta',
-                   filename     => "$gid.faa" );
+                   filename     => "$gid.$suffix{$type}" );
 };
 
 dance;
